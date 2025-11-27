3a7fcd4b2ebafc6909c03a39574af33af8433d68
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2025-11-27 06:12:16 +0100

    Add minimal flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+    };
+}
